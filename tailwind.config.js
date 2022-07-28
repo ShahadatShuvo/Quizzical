abd3262579a4ef599ad48cd47efb602ce7c66244
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-07-28 14:46:50 +0600

    feat[Working on Front Page design]App.jsx

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: {
+        light: '#D6DBF5',
+        DEFAULT: '#4D5B9E',
+        dark: '#293264',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#c0ccda',
+        light: '#e0e6ed',
+        lightest: '#f9fafc',
+      }
+    }
+  },
+  plugins: [],
+}
