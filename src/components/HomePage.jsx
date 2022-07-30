12071ef203ba91b60bdf202e652bb8b76efb8d9b
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-07-31 01:37:48 +0600

    code formatted

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,24 +1,32 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import { Button } from "@mui/material";
+import React from "react";
 
 function HomePage({ setQuizPage, fetchQuiz }) {
-    function onHandleClick() {
-        setQuizPage("quiz")
-        fetchQuiz();
-    }
-    return (
-        <div className="z-30 h-full w-full flex justify-center items-center" >
-            <div className="text-center leading-10">
-                <h1 className="text-4xl font-bold text-primary">Quizzical</h1>
-                <p className="text-primary font-bold">Check Your General Knowledge.</p>
-                <Button sx={{
-                    bgcolor: "#4D5B9E", "&:hover": {
-                        bgcolor: "#4D5B9E",
-                    }
-                }} variant="contained" size="small" onClick={onHandleClick}>Start Quiz</Button>
-            </div>
-        </div>
-    )
+  function onHandleClick() {
+    setQuizPage("quiz");
+    fetchQuiz();
+  }
+  return (
+    <div className="z-30 h-full w-full flex justify-center items-center">
+      <div className="text-center leading-10">
+        <h1 className="text-4xl font-bold text-primary">Quizzical</h1>
+        <p className="text-primary font-bold">Check Your General Knowledge.</p>
+        <Button
+          sx={{
+            bgcolor: "#4D5B9E",
+            "&:hover": {
+              bgcolor: "#4D5B9E",
+            },
+          }}
+          variant="contained"
+          size="small"
+          onClick={onHandleClick}
+        >
+          Start Quiz
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
